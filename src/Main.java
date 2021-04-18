90263e024e3f448763dd197cecb2fc7a33745a6a
SantiagoGuirado <santiagoadrianguirado@gmail.com>
2021-04-18 12:10:57 -0300

    Cambios en como se cargan las coordenadas
    Se modificó la condición para dejar de cargar coordenadas. En
    lugar de usar un try catch se un utiliza un if que cambia el
    valor de una variable booleana.

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,78 @@
 public class Main {
     //Atributos
-    static Puntos puntos;
-    static Leer leer;
-    static CalcularDistancia calcularDistancia;
+    private static Puntos puntos;
+    private static Leer leer;
+    private static CalcularDistancia calcularDistancia;
 
     //Metodos
     public static void main(String[] args) {
         implementarElementos();
+
         cargarPuntos();
+        getCalcularDistancia().calcularMenorDistancia(getPuntos());
         mostrarResultado();
     }
 
     public static void implementarElementos(){
-        puntos = new Puntos();
-        leer = new Leer();
-        calcularDistancia = new CalcularDistancia();
+        setPuntos(new Puntos());
+        setLeer(new Leer());
+        setCalcularDistancia(new CalcularDistancia());
     }
 
     public static void cargarPuntos(){
-        System.out.println("Ingrese las coordenadas de los puntos. Para dejar de cargar puntos ingrese una letra.");
+        System.out.println("Ingrese las coordenadas de los puntos.");
+        System.out.println("Para dejar de cargar puntos ingrese 0 en ambas coordenadas.");
         int contador = 1;
         boolean salida = false;
         do {
-            try{
-                System.out.println("Punto " + contador + ":");
-                System.out.println("X");
-                double x = leer.leerDouble();
-                System.out.println("Y");
-                double y = leer.leerDouble();
-                puntos.getCoordenadaX().add(x);
-                puntos.getCoordenadaY().add(y);
-            } catch (Exception e) {
+            System.out.println("Punto " + contador + ":");
+            System.out.println("X:");
+            double x = getLeer().leerDouble();
+            System.out.println("Y:");
+            double y = getLeer().leerDouble();
+
+            if (x == 0 && y == 0){
                 salida = true;
+            } else {
+                getPuntos().getCoordenadaX().add(x);
+                getPuntos().getCoordenadaY().add(y);
             }
             contador++;
-        } while (salida == false && contador < 201);
+        } while (!salida && contador < 201);
     }
 
     public static void mostrarResultado(){
-        calcularDistancia.calcularMenorDistancia(puntos);
-        System.out.println("La menor distancia es: " + calcularDistancia.getMenorDistancia());
-        System.out.println("Entre los puntos " + (calcularDistancia.getPunto1()+1) + " y " + (calcularDistancia.getPunto2()+1));
+        System.out.println("");
+        System.out.print("La menor distancia es: " + getCalcularDistancia().getMenorDistancia() + " ");
+        System.out.println("entre los puntos " + (getCalcularDistancia().getPunto1()+1) + " y " + (getCalcularDistancia().getPunto2()+1));
+        System.out.println("");
         System.out.println("Coordenadas");
-        System.out.println("Punto " + (calcularDistancia.getPunto1()+1) + ": X=" + puntos.getCoordenadaX().get(calcularDistancia.getPunto1()) + " Y=" + puntos.getCoordenadaY().get(calcularDistancia.getPunto1()) );
-        System.out.println("Punto " + (calcularDistancia.getPunto2()+1) + ": X=" + puntos.getCoordenadaX().get(calcularDistancia.getPunto2()) + " Y=" + puntos.getCoordenadaY().get(calcularDistancia.getPunto2()) );
+        System.out.println("Punto " + (getCalcularDistancia().getPunto1()+1) + ": X=" + getPuntos().getCoordenadaX().get(getCalcularDistancia().getPunto1()) + " Y=" + getPuntos().getCoordenadaY().get(getCalcularDistancia().getPunto1()) );
+        System.out.print("Punto " + (getCalcularDistancia().getPunto2()+1) + ": X=" + getPuntos().getCoordenadaX().get(getCalcularDistancia().getPunto2()) + " Y=" + getPuntos().getCoordenadaY().get(getCalcularDistancia().getPunto2()) );
+    }
+
+    //Setters && Getters
+    public static Puntos getPuntos() {
+        return puntos;
+    }
+
+    public static void setPuntos(Puntos puntos) {
+        Main.puntos = puntos;
+    }
+
+    public static Leer getLeer() {
+        return leer;
+    }
+
+    public static void setLeer(Leer leer) {
+        Main.leer = leer;
+    }
+
+    public static CalcularDistancia getCalcularDistancia() {
+        return calcularDistancia;
+    }
+
+    public static void setCalcularDistancia(CalcularDistancia calcularDistancia) {
+        Main.calcularDistancia = calcularDistancia;
     }
 }
